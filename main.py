95b97164bd977cea49b40d8bc64f404b73716882
dcee_ucsp <165704038+dcee-ucsp@users.noreply.github.com>
2025-02-17 11:41:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Coincidencias de horarios")
+
 col1, col2 = st.columns(2)
 with col1:
     file_path = "source/plantilla_cursos.xlsx"
@@ -8,14 +10,14 @@ with col1:
         file_bytes = file.read()
     
     st.download_button(
-        label="Descargar plantilla",
+        label="Descargar plantilla de base de cursos",
         data=file_bytes,
         file_name="plantilla_cursos.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
 
 with col2:
-    uploaded_file = st.file_uploader("Subir archivo", type=["xlsx", "csv"])
+    uploaded_file = st.file_uploader("Subir base de cursos", type=["xlsx", "csv"])
 
 if uploaded_file:
     try:
@@ -24,8 +26,3 @@ if uploaded_file:
             df = pd.read_excel(uploaded_file)
         elif uploaded_file.name.endswith(".csv"):
             df = pd.read_csv(uploaded_file)
-
-        st.write("Vista previa del archivo:")
-        st.dataframe(df)  # Mostrar el DataFrame
-    except Exception as e:
-        st.error(f"Error al procesar el archivo: {e}")
