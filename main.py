4bc416771e1d6983ebbb2dc8b9f0f993493ac7a4
dcee_ucsp <165704038+dcee-ucsp@users.noreply.github.com>
2025-02-17 11:35:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import streamlit as st
+import pandas as pd
 
-file_path = "source/plantilla_cursos.xlsx"
+col1, col2 = st.columns(2)
+with col1:
+    file_path = "source/plantilla_cursos.xlsx"
+    with open(file_path, "rb") as file:
+        file_bytes = file.read()
+    
+    st.download_button(
+        label="Descargar plantilla",
+        data=file_bytes,
+        file_name="plantilla_cursos.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
 
-with open(file_path, "rb") as file:
-    file_bytes = file.read()
+with col2:
+    uploaded_file = st.file_uploader("Subir archivo", type=["xlsx", "csv"])
 
-# Botón de plantilla
-st.download_button(
-    label="Descargar plantilla",
-    data=file_bytes,
-    file_name="plantilla_cursos.xlsx",
-    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-)
+if uploaded_file:
+    try:
+        # Detectar el tipo de archivo y convertirlo en un DataFrame
+        if uploaded_file.name.endswith(".xlsx"):
+            df = pd.read_excel(uploaded_file)
+        elif uploaded_file.name.endswith(".csv"):
+            df = pd.read_csv(uploaded_file)
+
+        st.write("Vista previa del archivo:")
+        st.dataframe(df)  # Mostrar el DataFrame
+    except Exception as e:
+        st.error(f"Error al procesar el archivo: {e}")
